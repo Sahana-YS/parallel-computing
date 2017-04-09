fa9e56cc1b26946f27a871b9b67877540a018579
Sahana-YS <sahana.ys2@gmail.com>
2017-04-09 10:30:00 +0530

    Create memset-basic example.cpp

---
+++ Memset/memset-basic example.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include <omp.h>
+
+using namespace std;
+
+int main()
+{
+int A[100000], i;
+clock_t tic, toc;
+ 
+tic = clock();
+for (int j=0; j<10000; j++) for (i=0; i<100000; i++) A[i]=j;
+toc = clock();
+cout << "Without memset:"<< (toc-tic)*1.0/CLOCKS_PER_SEC << endl;
+
+tic = clock();
+for (int j=0; j<10000; j++) memset(A, j, sizeof(A));
+toc = clock();
+cout << "With memset:"<<(toc-tic)*1.0/CLOCKS_PER_SEC << endl;
+
+return 0;
+}
+
+/**
+sahana@Sahana-Inspiron-3543:~/VI Semester/Parallel Computing/Blog/Post 2$ g++ memset.cpp 
+sahana@Sahana-Inspiron-3543:~/VI Semester/Parallel Computing/Blog/Post 2$ ./a.out 
+Without memset:2.70535
+With memset:0.157259
+**/
