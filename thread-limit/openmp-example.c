f239ae3d21fa50c83c79ce34df9bce7b5787876b
Sahana-YS <sahana.ys2@gmail.com>
2017-04-19 21:44:34 +0530

    Update openmp-example.c

--- thread-limit/openmp-example.c
+++ thread-limit/openmp-example.c
@@ -5,19 +5,25 @@
 int main()
 {
     int i, *a;
-    long long sum = 0;
+    int sum = 0;
     double t;
     a = (int*)malloc(N*sizeof(int));
     t = omp_get_wtime();
-#pragma omp parallel shared( a, sum )
-    {
-#pragma omp for
+    //Parallel sections - The initialization and finding sum is done in parallel
+   #pragma omp parallel shared(a, sum)
+   {
+    #pragma omp for
    for( i=0; i<N; ++i)
-       { a[i] = i; }
-#pragma omp for reduction(+:sum)
-   for( i=0; i<N; ++i)
-       { sum += a[i]; }
+    { 
+       a[i] = i; 
     }
-    printf( "sum =%lld, t = %f\n", sum, omp_get_wtime()-t );
+    #pragma omp for reduction(+:sum)
+   for( i=0; i<N; ++i)
+    {
+        sum += a[i]; 
+    }
+  }
+    //Prints the sum and the time taken in the parallel section
+    printf( "sum =%d, t = %f\n", sum, omp_get_wtime()-t );
     return 0;
 }
