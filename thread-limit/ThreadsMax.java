80ef51d1344f5c42ed0e43e46ae6fcc8b9c52726
Sahana-YS <sahana.ys2@gmail.com>
2017-04-19 21:42:38 +0530

    Update ThreadsMax.java

--- thread-limit/ThreadsMax.java
+++ thread-limit/ThreadsMax.java
@@ -1,26 +1,33 @@
+//Example to demonstrate maximum number of threads in java 
+//Main class
 public class ThreadsMax {
+    public static void main(String[] argv){
+        //Infinite loop - Creates threads and calls the run method of the thread class
+        for(;;){
+            new Thread(new Worker()).start();
+        }
+    }
+}
+
+class Worker extends Thread
+{
     private static Object s = new Object();
     private static int count = 0;
-    public static void main(String[] argv){
+    public void run()
+    {
+        //When a thread executes the below statement, it acquires a lock on object s and unitil it releases the 
+        //lock, another thread cannot lock the object and the first thread releases the lock only after it 
+        //finishes executing the block which follows it. 
+        synchronized(s){
+           count += 1;
+           System.err.println("New thread #"+count);
+        }
         for(;;){
-            new Thread(new Runnable(){
-                    public void run(){
-                    //When a thread executes the below statement, it acquires a lock on object s and unitil it releases the 
-                    //lock, another thread cannot lock the object and the first thread releases the lock only after it 
-                    //finishes executing the block which follows it. 
-                        synchronized(s){
-                            count += 1;
-                            System.err.println("New thread #"+count);
-                        }
-                        for(;;){
-                            try {
-                                Thread.sleep(1000);
-                            } catch (Exception e){
-                                System.err.println(e);
-                            }
-                        }
-                    }
-                }).start();
+           try {
+              Thread.sleep(1000);
+           } catch (Exception e){
+              System.err.println(e);
+           }
         }
     }
 }
